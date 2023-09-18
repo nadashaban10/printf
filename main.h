71f29af5933b159948bf9474aae00399a909387e
nadashabn10 <nadashaban10@github.com>
2023-09-18 04:08:55 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#define BUFF_SIZE 1024
+
+/* FUNTION PROTOTYPES */
+
+int _printf(const char *format, ...);
+int char_func(va_list case, char buffer[]);
+int str_func(va_list case, char buffer[]);
+int percentage_func(va_list case, char buffer[]);
+int num_func(va_list case, buffer[]);
+int binary_func(va_list case, buffer[]);
+
+
+
+
+#endif
