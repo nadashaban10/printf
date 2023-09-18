3121358fbca7bbf42f1eaa2e902233c32898bf61
nadashabn10 <nadashaban10@github.com>
2023-09-18 05:24:19 -0700

    0-functions file

---
+++ 0-function.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ *char_func - function that print a char
+ *@cases: list of argu
+ *@buffer: array buffer
+ *Return: number of char printed
+ */
+
+
+int char_func(va_list cases, char buffer[])
+{
+	char c;
+
+	c = va_arg(cases, int);
+	return (write_char_(c, buffer));	/*func isn't craet yet!*/
+}
+
+/**
+ *percentage_func - funtion that print percent sign
+ *@cases:list of argu
+ *@buffer:buffer array
+ *Return: number of char printed
+ */
+
+int percentage_func(va_list cases, char buffer[])
+{
+	UNUSED(cases);
+	UNUSED(buffer);
+	return (write(1, "%%", 1));
+}
+
+
+/**
+*string_func - function that print length of char printed
+*@buffer: buffer array
+*@cases: list of argu
+*Return: number of char printed
+*/
+
+int string_func(va_list cases, char buffer[])
+{
+int leng;
+char *str;
+
+leng = 0;
+str = va_arg(cases, char *);
+UNUSED(buffer);
+while (str[leng] != '\0')
+	leng++;
+
+return (write(1, str, leng));
+}
