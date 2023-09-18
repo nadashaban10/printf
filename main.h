d6272efe6988ac76e14149b7822a739d52a2ae37
SoriaAbd <soriaabd1994@gmail.com>
2023-09-18 08:52:30 -0700

    edit main

--- main.h
+++ main.h
@@ -15,7 +15,9 @@ int percentage_func(va_list case, char buffer[]);
 int num_func(va_list case, buffer[]);
 int binary_func(va_list case, buffer[]);
 void buffer_print(char buffer[], int *index_buffer); /*using in printf file*/
-
-
-
+int print_i(va_list cases, char buffer[], int f, int w, int p, int s);
+int write_digit(int is_negative, int index, char buffer[],
+		int f, int w, int p, int s);
+int write_numb(int index, char buffer[], int f, int w,
+		int p, int len, char pading, char ext_c);
 #endif
