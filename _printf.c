58f754e1f0677b91cadd247d627e9462bed46a25
nadashabn10 <nadashaban10@github.com>
2023-09-19 10:38:08 -0700

    solving

--- _printf.c
+++ _printf.c
@@ -30,26 +30,25 @@ int _printf(const char *format, ...)
 			if (index_buffer == BUFF_SIZE)
 
 				ind_printed_c++;
-			}
-			else
-			{
+		}
+		else
+		{
 			buffer_print(buffer, &index_buffer);
 			flag = g_flag(format, &i);
 			s_width = get_w(format, &i, list);
 			precision = g_precision(format, &i, list);
 			s_size = get_s(format, &i);
-				i++;
-				ind_printed = handle_fun_print(format, &i, list, buffer, flag, s_width, 
-						precision, s_size);
-					if (ind_printed == -1)
-					{
-						return (-1);
-						ind_printed_c = ind_printed_c + ind_printed;
+			i++;
+			ind_printed = handle_fun_print(format, &i, list, buffer, flag, s_width, 
+					precision, s_size);
+			if (ind_printed == -1)
+			{
+				return (-1);
+				ind_printed_c = ind_printed_c + ind_printed;
 			}	
 		}
 		buffer_print(buffer, &index_buffer);
 		va_end(list);
-		return (ind_printed_c);
-
-}
+	}
+	return (ind_printed_c);
 }
