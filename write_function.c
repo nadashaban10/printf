830684c19179c61b41fe0ac8610a126e8a91719d
SoriaAbd <soriaabd1994@gmail.com>
2023-09-19 03:41:27 -0700

    finish write fun

--- write_function.c
+++ write_function.c
@@ -124,3 +124,92 @@ int write_char_(char c, char buffer[], int f, int w, int p, int s)
 
 	return (write(1, &buffer[0], 1));
 }
+/**
+ * write_unsnum - write unsigned number
+ * @is_negative:check if number is negative
+ * @index:when we start
+ * @buffer:an array
+ * @f:flages
+ * @w:width
+ * @p:precision
+ * @s:size
+ * Return:unsigned number
+ * develop by soria
+ */
+int write_unsnum(int is_negative, int index, char buffer[],
+		int f, int w, int p, int s)
+{
+	int len = BUFF_SIZE - index - 1, x = 0;
+	char pading = ' ';
+
+	UNUSED(is_negative);
+	UNUSED(s);
+	if (p == 0 && index == BUFF_SIZE - 2 && buffer[index] == '0')
+		return (0);
+	if (p > 0 && p < len)
+		pading = ' ';
+	while (p > len)
+		buffer[--index] = '0', len++;
+	if ((f & F_ZERO) && !(f & F_MINUS))
+		pading = '0';
+
+	if (w > len)
+	{
+		for (x = 0 ; x < w - len ; x++)
+			buffer[x] = pading;
+		buffer[x] = '\0';
+
+		if (f & F_MINUS)
+			return (write(1, &buffer[index], len) + write(1, &buffer[0], x));
+		else
+			return (write(1, &buffer[0], x) + write(1, &buffer[index], len));
+	}
+	return (write(1, &buffer[index], len));
+}
+/**
+ * write_point - write a pointer.
+ * @buffer:an array
+ * @index:when we start
+ * @len:length
+ * @w:width
+ * @f:flages
+ * @pading:pading char
+ * @ext_c:extra char
+ * @pading_start:when pading start
+ * Return:a pointer like char
+ * develop by soria
+ */
+int write_point(char buffer[], int index, int len, int w,
+		int f, char pading, char ext_c, int pading_start)
+{
+	int x;
+
+	if (w > len)
+	{
+		for (x = 3 ; x < w - len + 3 ; x++)
+			buffer[x] = pading;
+		buffer[x] = '\0';
+		if (f & F_MINUS && pading == ' ')
+		{
+			buffer[--index] = 'x';
+			buffer[--index] = '0';
+			if (ext_c)
+				buffer[--index] = ext_c;
+			return (write(1, &buffer[index], len) + write(1, &buffer[3], x - 3));
+		}
+		else if (!(f & F_MINUS) && pading == '0')
+		{
+			if (ext_c)
+				buffer[--pading_start] = ext_c;
+			buffer[1] = '0';
+			buffer[2] = 'x';
+			return (write(1, &buffer[pading_start], x - pading_start) +
+					write(1, &buffer[index], len - (1 - pading_start) - 2));
+		}
+	}
+	buffer[--index] = 'x';
+	buffer[--index] = '0';
+	if (ext_c)
+		buffer[--index] = ext_c;
+	return (write(1, &buffer[index], BUFF_SIZE - index - 1));
+}
