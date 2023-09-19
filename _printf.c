559ffae1e6ccd3e033e10aeb34b64a6b4ecc2cc0
SoriaAbd <soriaabd1994@gmail.com>
2023-09-19 12:40:50 -0700

    o

--- _printf.c
+++ _printf.c
@@ -71,4 +71,18 @@ int _printf(const char *format, ...)
 	}
 
 		return (ind_printed_c);
-		}
+}
+/**
+ *
+ *
+ *
+ *
+ */
+
+void buffer_print(char buffer, int &index_buffer)
+{
+	if (*index_buffer > 0)
+		write(1, &buffer[0], *index_buffer);
+
+	*index_buffer = 0;
+}
